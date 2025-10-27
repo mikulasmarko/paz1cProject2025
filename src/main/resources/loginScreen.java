6dc5a0a765423ac1a769c991bcef61246e953739
mmark <5410498@upjs.sk>
2025-10-27 13:01:55 +0100

    dal som prvu obrazofku

---
+++ src/main/resources/loginScreen.java
@@ -0,0 +1,26 @@
+package org;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class example {
+
+    @FXML
+    private Button darkLightButton;
+
+    @FXML
+    private Button languageButtonEN;
+
+    @FXML
+    private Button languageButtonSK;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button registrationButton;
+
+    @FXML
+    private Button settingsButton;
+
+}
