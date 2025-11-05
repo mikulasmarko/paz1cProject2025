fc4c064124afa7a80a6304a2f82c9b4cbaa74c43
mmark <5410498@upjs.sk>
2025-11-05 22:52:58 +0100

    ukladanie qr v png nie lokalne ale do projektovej struktury

--- src/main/java/org/example/QrCode/QrCodeGeneratorDemo.java
+++ src/main/java/org/example/QrCode/QrCodeGeneratorDemo.java
@@ -39,19 +39,19 @@ import javax.imageio.ImageIO;
 
 public final class QrCodeGeneratorDemo {
 
-	// The main application program.
-	public static void main(String[] args) throws IOException {
-		//doBasicDemo();
-		doVarietyDemo();
-		//doSegmentDemo();
-		//doMaskDemo();
-	}
+    // The main application program.
+    public static void main(String[] args) throws IOException {
+        //doBasicDemo();
+        doVarietyDemo();
+        //doSegmentDemo();
+        //doMaskDemo();
+    }
 
 
 
-	/*---- Demo suite ----*/
+    /*---- Demo suite ----*/
 
-	// Creates a single QR Code, then writes it to a PNG file and an SVG file.
+    // Creates a single QR Code, then writes it to a PNG file and an SVG file.
 //	private static void doBasicDemo() throws IOException {
 //		String text = "Hello, world!";          // User-supplied Unicode text
 //		QrCode.Ecc errCorLvl = QrCode.Ecc.LOW;  // Error correction level
@@ -69,13 +69,13 @@ public final class QrCodeGeneratorDemo {
 //	}
 
 
-	// Creates a variety of QR Codes that exercise different features of the library, and writes each one to file.
-	private static void doVarietyDemo() throws IOException {
-		QrCode qr;
-		String cisloZakaznika = "540678";
-		// Numeric mode encoding (3.33 bits per digit)
-		qr = QrCode.encodeText(cisloZakaznika, QrCode.Ecc.MEDIUM);
-		writePng(toImage(qr, 13, 1), cisloZakaznika + ".png");
+    // Creates a variety of QR Codes that exercise different features of the library, and writes each one to file.
+    private static void doVarietyDemo() throws IOException {
+        QrCode qr;
+        String cisloZakaznika = "540678";
+        // Numeric mode encoding (3.33 bits per digit)
+        qr = QrCode.encodeText(cisloZakaznika, QrCode.Ecc.MEDIUM);
+        writePng(toImage(qr, 13, 1), "src/main/resources/qrCodesGenerated/" + cisloZakaznika + ".png");
 
 //		// Alphanumeric mode encoding (5.5 bits per character)
 //		qr = QrCode.encodeText("DOLLAR-AMOUNT:$39.87 PERCENTAGE:100.00% OPERATIONS:+-*/", QrCode.Ecc.HIGH);
@@ -95,10 +95,10 @@ public final class QrCodeGeneratorDemo {
 //						+ "daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly "
 //						+ "a White Rabbit with pink eyes ran close by her.", QrCode.Ecc.HIGH);
 //		writePng(toImage(qr, 6, 10), "alice-wonderland-QR.png");
-	}
+    }
 
 
-	// Creates QR Codes with manually specified segments for better compactness.
+    // Creates QR Codes with manually specified segments for better compactness.
 //	private static void doSegmentDemo() throws IOException {
 //		QrCode qr;
 //		List<QrSegment> segs;
@@ -166,91 +166,93 @@ public final class QrCodeGeneratorDemo {
 
 
 
-	/*---- Utilities ----*/
+    /*---- Utilities ----*/
 
-	private static BufferedImage toImage(QrCode qr, int scale, int border) {
-		return toImage(qr, scale, border, 0xFFFFFF, 0x000000);
-	}
-
-
-	/**
-	 * Returns a raster image depicting the specified QR Code, with
-	 * the specified module scale, border modules, and module colors.
-	 * <p>For example, scale=10 and border=4 means to pad the QR Code with 4 light border
-	 * modules on all four sides, and use 10&#xD7;10 pixels to represent each module.
-	 * @param qr the QR Code to render (not {@code null})
-	 * @param scale the side length (measured in pixels, must be positive) of each module
-	 * @param border the number of border modules to add, which must be non-negative
-	 * @param lightColor the color to use for light modules, in 0xRRGGBB format
-	 * @param darkColor the color to use for dark modules, in 0xRRGGBB format
-	 * @return a new image representing the QR Code, with padding and scaling
-	 * @throws NullPointerException if the QR Code is {@code null}
-	 * @throws IllegalArgumentException if the scale or border is out of range, or if
-	 * {scale, border, size} cause the image dimensions to exceed Integer.MAX_VALUE
-	 */
-	private static BufferedImage toImage(QrCode qr, int scale, int border, int lightColor, int darkColor) {
-		Objects.requireNonNull(qr);
-		if (scale <= 0 || border < 0)
-			throw new IllegalArgumentException("Value out of range");
-		if (border > Integer.MAX_VALUE / 2 || qr.size + border * 2L > Integer.MAX_VALUE / scale)
-			throw new IllegalArgumentException("Scale or border too large");
-
-		BufferedImage result = new BufferedImage((qr.size + border * 2) * scale, (qr.size + border * 2) * scale, BufferedImage.TYPE_INT_RGB);
-		for (int y = 0; y < result.getHeight(); y++) {
-			for (int x = 0; x < result.getWidth(); x++) {
-				boolean color = qr.getModule(x / scale - border, y / scale - border);
-				result.setRGB(x, y, color ? darkColor : lightColor);
-			}
-		}
-		return result;
-	}
+    private static BufferedImage toImage(QrCode qr, int scale, int border) {
+        return toImage(qr, scale, border, 0xFFFFFF, 0x000000);
+    }
 
 
-	// Helper function to reduce code duplication.
-	private static void writePng(BufferedImage img, String filepath) throws IOException {
-		ImageIO.write(img, "png", new File(filepath));
-	}
+    /**
+     * Returns a raster image depicting the specified QR Code, with
+     * the specified module scale, border modules, and module colors.
+     * <p>For example, scale=10 and border=4 means to pad the QR Code with 4 light border
+     * modules on all four sides, and use 10&#xD7;10 pixels to represent each module.
+     *
+     * @param qr         the QR Code to render (not {@code null})
+     * @param scale      the side length (measured in pixels, must be positive) of each module
+     * @param border     the number of border modules to add, which must be non-negative
+     * @param lightColor the color to use for light modules, in 0xRRGGBB format
+     * @param darkColor  the color to use for dark modules, in 0xRRGGBB format
+     * @return a new image representing the QR Code, with padding and scaling
+     * @throws NullPointerException     if the QR Code is {@code null}
+     * @throws IllegalArgumentException if the scale or border is out of range, or if
+     *                                  {scale, border, size} cause the image dimensions to exceed Integer.MAX_VALUE
+     */
+    private static BufferedImage toImage(QrCode qr, int scale, int border, int lightColor, int darkColor) {
+        Objects.requireNonNull(qr);
+        if (scale <= 0 || border < 0)
+            throw new IllegalArgumentException("Value out of range");
+        if (border > Integer.MAX_VALUE / 2 || qr.size + border * 2L > Integer.MAX_VALUE / scale)
+            throw new IllegalArgumentException("Scale or border too large");
+
+        BufferedImage result = new BufferedImage((qr.size + border * 2) * scale, (qr.size + border * 2) * scale, BufferedImage.TYPE_INT_RGB);
+        for (int y = 0; y < result.getHeight(); y++) {
+            for (int x = 0; x < result.getWidth(); x++) {
+                boolean color = qr.getModule(x / scale - border, y / scale - border);
+                result.setRGB(x, y, color ? darkColor : lightColor);
+            }
+        }
+        return result;
+    }
 
 
-	/**
-	 * Returns a string of SVG code for an image depicting the specified QR Code, with the specified
-	 * number of border modules. The string always uses Unix newlines (\n), regardless of the platform.
-	 * @param qr the QR Code to render (not {@code null})
-	 * @param border the number of border modules to add, which must be non-negative
-	 * @param lightColor the color to use for light modules, in any format supported by CSS, not {@code null}
-	 * @param darkColor the color to use for dark modules, in any format supported by CSS, not {@code null}
-	 * @return a string representing the QR Code as an SVG XML document
-	 * @throws NullPointerException if any object is {@code null}
-	 * @throws IllegalArgumentException if the border is negative
-	 */
-	private static String toSvgString(QrCode qr, int border, String lightColor, String darkColor) {
-		Objects.requireNonNull(qr);
-		Objects.requireNonNull(lightColor);
-		Objects.requireNonNull(darkColor);
-		if (border < 0)
-			throw new IllegalArgumentException("Border must be non-negative");
-		long brd = border;
-		StringBuilder sb = new StringBuilder()
-				.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-				.append("<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n")
-				.append(String.format("<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" viewBox=\"0 0 %1$d %1$d\" stroke=\"none\">\n",
-						qr.size + brd * 2))
-				.append("\t<rect width=\"100%\" height=\"100%\" fill=\"" + lightColor + "\"/>\n")
-				.append("\t<path d=\"");
-		for (int y = 0; y < qr.size; y++) {
-			for (int x = 0; x < qr.size; x++) {
-				if (qr.getModule(x, y)) {
-					if (x != 0 || y != 0)
-						sb.append(" ");
-					sb.append(String.format("M%d,%dh1v1h-1z", x + brd, y + brd));
-				}
-			}
-		}
-		return sb
-				.append("\" fill=\"" + darkColor + "\"/>\n")
-				.append("</svg>\n")
-				.toString();
-	}
+    // Helper function to reduce code duplication.
+    private static void writePng(BufferedImage img, String filepath) throws IOException {
+        ImageIO.write(img, "png", new File(filepath));
+    }
+
+
+    /**
+     * Returns a string of SVG code for an image depicting the specified QR Code, with the specified
+     * number of border modules. The string always uses Unix newlines (\n), regardless of the platform.
+     *
+     * @param qr         the QR Code to render (not {@code null})
+     * @param border     the number of border modules to add, which must be non-negative
+     * @param lightColor the color to use for light modules, in any format supported by CSS, not {@code null}
+     * @param darkColor  the color to use for dark modules, in any format supported by CSS, not {@code null}
+     * @return a string representing the QR Code as an SVG XML document
+     * @throws NullPointerException     if any object is {@code null}
+     * @throws IllegalArgumentException if the border is negative
+     */
+    private static String toSvgString(QrCode qr, int border, String lightColor, String darkColor) {
+        Objects.requireNonNull(qr);
+        Objects.requireNonNull(lightColor);
+        Objects.requireNonNull(darkColor);
+        if (border < 0)
+            throw new IllegalArgumentException("Border must be non-negative");
+        long brd = border;
+        StringBuilder sb = new StringBuilder()
+                .append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+                .append("<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n")
+                .append(String.format("<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" viewBox=\"0 0 %1$d %1$d\" stroke=\"none\">\n",
+                        qr.size + brd * 2))
+                .append("\t<rect width=\"100%\" height=\"100%\" fill=\"" + lightColor + "\"/>\n")
+                .append("\t<path d=\"");
+        for (int y = 0; y < qr.size; y++) {
+            for (int x = 0; x < qr.size; x++) {
+                if (qr.getModule(x, y)) {
+                    if (x != 0 || y != 0)
+                        sb.append(" ");
+                    sb.append(String.format("M%d,%dh1v1h-1z", x + brd, y + brd));
+                }
+            }
+        }
+        return sb
+                .append("\" fill=\"" + darkColor + "\"/>\n")
+                .append("</svg>\n")
+                .toString();
+    }
 
 }
 
