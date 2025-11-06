12ebda7e6003f167e4617c4079136d5928d2d6b7
mmark <5410498@upjs.sk>
2025-11-06 14:53:28 +0100

    tomasjedno mail

--- src/main/java/org/example/eMail/emailSender.java
+++ src/main/java/org/example/eMail/emailSender.java
@@ -8,20 +8,22 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
+import static java.lang.Thread.sleep;
+
 public class emailSender {
 
-    private static final String emailFrom = "tomasjedno.noreply@gmail.com";
-    private static final String emailTo = "matus.klimko@student.upjs.sk";
+    private static final String emailFrom = "to.mas.jedno.noreply@gmail.com";
+    private static final String emailTo = "matej.grofcik@student.upjs.sk";
     private static final String appPassword = "djsv akwy biqp tcyr";
 
-    public static void main(String[] args) throws MessagingException, IOException {
+    public static void main(String[] args) throws MessagingException, IOException, InterruptedException {
         int i = 0;
-        while (i != 15) {
+        while (i != 29) {
 
             Message message = new MimeMessage(getEmailSession());
             message.setFrom(new InternetAddress(emailFrom));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailTo));
-            message.setSubject("jebem ti mater");
+            message.setSubject("mame ta radi matej");
 
 //            BodyPart messageBodyPart = new MimeBodyPart();
 //            messageBodyPart.setText("attachment test");
@@ -40,10 +42,11 @@ public class emailSender {
                     ked sa na teba pozriem
                     moja laska k tebe je nekonecna
                     ahhhhhh
-                    matusko""");
+                    matejkoooo pan vcielka""");
             Transport.send(message);
             System.out.println("email sent, cislo: " + (i + 1));
             i++;
+//            sleep(1000*60);
         }
     }
 
