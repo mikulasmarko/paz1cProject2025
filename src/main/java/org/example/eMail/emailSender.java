d6245d0da1963b11f87e0fd90b425d36a6b90c3d
Matúš Klimko <matklim09@gmail.com>
2025-11-27 13:11:38 +0100

    registration button functionality

--- src/main/java/org/example/eMail/emailSender.java
+++ src/main/java/org/example/eMail/emailSender.java
@@ -11,53 +11,51 @@ import static org.example.eMail.emailConstants.*;
 
 public class emailSender {
 
-    private static final String emailFrom = APP_MAIL;
-    private static final String emailTo = "matus.klimko@student.upjs.sk";
-    private static final String appPassword = EMAIL_APP_PASS;
+	private static final String emailFrom = APP_MAIL;
+	private static final String emailTo = "5388467@upjs.sk";
+	private static final String appPassword = EMAIL_APP_PASS;
 
-    public static void main(String[] args) throws MessagingException, IOException {
+	public static void main(String[] args) throws MessagingException, IOException {
 
+		Message message = new MimeMessage(getEmailSession());
+		message.setFrom(new InternetAddress(emailFrom));
+		message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailTo));
+		message.setSubject("Registrácia");
 
-        Message message = new MimeMessage(getEmailSession());
-        message.setFrom(new InternetAddress(emailFrom));
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailTo));
-        message.setSubject("Registrácia");
+		BodyPart messageBodyPart = new MimeBodyPart();
+		messageBodyPart.setText("Registrácia úspešná");
 
-        BodyPart messageBodyPart = new MimeBodyPart();
-        messageBodyPart.setText("Registrácia úspešná");
+		MimeBodyPart attachmentPart = new MimeBodyPart();
+		attachmentPart.attachFile(new File("src/main/resources/qrCodesGenerated/540678.png"));
 
-        MimeBodyPart attachmentPart = new MimeBodyPart();
-        attachmentPart.attachFile(new File("src/main/resources/qrCodesGenerated/540678.png"));
+		Multipart multipart = new MimeMultipart();
+		multipart.addBodyPart(messageBodyPart);
+		multipart.addBodyPart(attachmentPart);
 
-        Multipart multipart = new MimeMultipart();
-        multipart.addBodyPart(messageBodyPart);
-        multipart.addBodyPart(attachmentPart);
+		message.setContent(multipart);
 
-        message.setContent(multipart);
+		Transport.send(message);
+		System.out.println("email sent");
 
-        Transport.send(message);
-        System.out.println("email sent");
+	}
 
+	private static Session getEmailSession() {
+		return Session.getInstance(getEmailProperties(), new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(emailFrom, appPassword);
+			}
+		});
+	}
 
-    }
-
-    private static Session getEmailSession() {
-        return Session.getInstance(getEmailProperties(), new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(emailFrom, appPassword);
-            }
-        });
-    }
-
-    private static Properties getEmailProperties() {
-        Properties properties = new Properties();
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.port", "587");
-        properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-        return properties;
-    }
+	private static Properties getEmailProperties() {
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.port", "587");
+		properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+		return properties;
+	}
 
 }
